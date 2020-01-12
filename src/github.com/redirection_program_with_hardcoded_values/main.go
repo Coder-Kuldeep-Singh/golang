030577c47f5a32f76416d1e600e10af75f36a770
mavensingh <kuldeep.avsar@gmail.com>
2020-01-12 21:27:43 +0530

    redirection: updated regex

--- src/github.com/redirection_program_with_hardcoded_values/main.go
+++ src/github.com/redirection_program_with_hardcoded_values/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	"io/ioutil"
+	"net/http"
 	"regexp"
 	_ "strings"
 )
@@ -133,7 +134,7 @@ func FetchData() {
 	Domain_List[100] = "http://givearide.us"
 	Domain_List[101] = "http://v2.tutree.com/"
 	Domain_List[102] = "http://51talkus.com"
-	Domain_List[103] = "http://httpqkids.co/"
+	Domain_List[103] = "https://qkids.co/"
 	Domain_List[104] = "http://rouchi.us/"
 	Domain_List[105] = "http://sprout4future.us/"
 	Domain_List[106] = "http://delivery.givearide.us/"
@@ -149,33 +150,67 @@ func FetchData() {
 			// fmt.Println("<xmp>")
 
 			// Execute Command to collect Data
-			out, err := exec.Command("curl", data_store1+data_store2).Output()
+			// out, err := exec.Command("curl", data_store1+data_store2).Output()
+			out, err := http.Get(data_store1 + data_store2)
 			if err != nil {
 				fmt.Println(err)
 			}
-			output := string(out)
-			re := regexp.MustCompile(`location.href=(.*);`)
-			// re := regexp.MustCompile(`url=(.*)" />`)
-			submatchall := re.FindAllStringSubmatch(output, -1)
-			//fmt.Println(submatchall);
-			for _, element := range submatchall {
-				fmt.Println(element[1])
+			// Print the HTTP Status Code and Status Name
+			// fmt.Println("HTTP Response Status:", out.StatusCode, http.StatusText(out.StatusCode))
+
+			if out.StatusCode >= 200 && out.StatusCode <= 299 {
+				// fmt.Println("HTTP Status is in the 2xx range")
+				body, err := ioutil.ReadAll(out.Body)
+				if err != nil {
+					fmt.Println(err)
+				}
+				output := string(body)
+				// re := regexp.MustCompile(`location.href(.*);`)
+				re := regexp.MustCompile(`content="5;(.*)"`)
+				// re := regexp.MustCompile(`http://(.*)`)
+				submatchall := re.FindAllStringSubmatch(output, -1)
+				for _, element := range submatchall {
+					fmt.Println(element[1])
+				}
+			} else {
+				// fmt.Println("Argh! Broken")
+				fmt.Println("HTTP Response Status:", out.StatusCode, http.StatusText(out.StatusCode))
 			}
 
 		}
 		// fmt.Println("***********************************************************************************************")
 		fmt.Println("<h2>" + "Ping" + "</h2>")
 		// Collect data to see versions
-		out, err := exec.Command("curl", data_store1+"/open-positions/ping").Output()
+		//out, err := exec.Command("curl", data_store1+"/open-positions/ping").Output()
+		out, err := http.Get(data_store1 + "/open-positions/ping")
 		if err != nil {
 			fmt.Println(err)
 		}
-		output := string(out)
-		ping_regex := regexp.MustCompile(`Version:(.*),`)
-		matchall := ping_regex.FindAllStringSubmatch(output, -1)
-		for _, element := range matchall {
-			fmt.Println("<ul>" + "<li>" + element[1] + "</li>" + "</ul>")
+
+		// fmt.Println("HTTP Response Status:", out.StatusCode, http.StatusText(out.StatusCode))
+
+		if out.StatusCode >= 200 && out.StatusCode <= 299 {
+			// fmt.Println("HTTP Status is in the 2xx range")
+			body, err := ioutil.ReadAll(out.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+			output := string(body)
+			ping_regex := regexp.MustCompile(`Version:(.*),`)
+			matchall := ping_regex.FindAllStringSubmatch(output, -1)
+			for _, element := range matchall {
+				fmt.Println("<ul>" + "<li>" + element[1] + "</li>" + "</ul>")
+			}
+
+		} else {
+			// fmt.Println("Argh! Broken")
+			fmt.Println("HTTP Response Status:", out.StatusCode, http.StatusText(out.StatusCode))
 		}
+
+		//output := string(out)
+		//
+		//
+		//
 		//output := string(out[20:36]) // Select range
 		// fmt.Println(data_store1)
 		fmt.Println("<hr/>")
