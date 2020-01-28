1e64f1b522f05813bf996bc1f9faf2bcb50c894b
mavensingh <kuldeep.avsar@gmail.com>
2020-01-28 16:04:00 +0530

    completed

--- src/github.com/img_crawler/main.go
+++ src/github.com/img_crawler/main.go
@@ -6,9 +6,10 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
-func Domains() {
+func Domains(w http.ResponseWriter, req *http.Request) {
 	//fetch data from given url
 	// domain := os.Args
 	response, err := http.Get("https://chaufferjob.us/robots.txt")
@@ -73,6 +74,7 @@ func Domains() {
 					log.Fatal(err)
 				}
 				storeDomains := string(body)
+				// re := regexp.MustCompile(`<img[^>]+\bsrc=["']([^"']+)["']`)
 				re := regexp.MustCompile(`<img[^>]+\bsrc=["']([^"']+)["']`)
 				// re := regexp.MustCompile(`http://(.*)`)
 				submatchall := re.FindAllStringSubmatch(storeDomains, -1)
@@ -81,18 +83,47 @@ func Domains() {
 				out := make([]string, len(submatchall))
 				for i := range out {
 					out[i] = submatchall[i][1]
-					fmt.Println(out[i])
-				}
-			}
 
+					storeDomains := string(out[i])
+					// re := regexp.MustCompile(`https://chaufferjob.us/(.*)`)
+					// re := regexp.MustCompile(`http://(.*)`)
+					// re := strings.TrimRight(storeDomains, "https://chaufferjobs.us")
+					re := strings.Replace(storeDomains, "https://chaufferjob.us", "", -1)
+					// outs := make([]string, len(re))
+					// fmt.Fprintln(w, re)
+					// for _, i := range re {
+					// fmt.Fprintln(w, i[1])
+					response, err := http.Get("https://chaufferjob.us" + re)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if response.StatusCode >= 200 && response.StatusCode <= 299 {
+						fmt.Fprintln(w, " Not Broken")
+					} else {
+						fmt.Fprintln(w, re+" Broken Images")
+					}
+					// defer response.Body.Close()
+					// // Read data from url
+					// body, err := ioutil.ReadAll(response.Body)
+					// if err != nil {
+					// 	log.Fatal(err)
+					// }
+					// storeDomains = string(body)
+					// fmt.Fprintln(w, storeDomains)
+
+				}
+				fmt.Fprintln(w, "**************************************************************************************")
+
+			}
 		}
 
 	}
+
 }
 
 func main() {
-	Domains()
-	// http.HandleFunc("/v1/domains", Domains)
-	// fmt.Println("Development server started localhost:8000/v1/domains")
-	// log.Fatal(http.ListenAndServe(":8000", nil))
+	// Domains()
+	http.HandleFunc("/v1/domains", Domains)
+	fmt.Println("Development server started localhost:8000/v1/domains")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
