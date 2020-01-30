b9ddf65a8f66e3e12775f86f21c87234445c61d1
mavensingh <kuldeep.avsar@gmail.com>
2020-01-30 19:31:13 +0530

    changed method of image taking

--- src/github.com/img_crawler/main.go
+++ src/github.com/img_crawler/main.go
@@ -15,10 +15,12 @@ import (
 func Domains() {
 	//fetch data from given url
 	// domain := os.Args
-	response, err := http.Get("https://chaufferjob.us/robots.txt")
+	response, err := http.Get("http://s.tutree.com:7635/v1/octodomains")
 	if err != nil {
 		log.Fatal(err)
 	}
+	// fmt.Fprintln(w, url)
+	// fmt.Fprintln(w, "***********************************************************************************************")
 	defer response.Body.Close()
 	// Read data from url
 	body, err := ioutil.ReadAll(response.Body)
@@ -26,12 +28,13 @@ func Domains() {
 		log.Fatal(err)
 	}
 	storeDomains := string(body)
-	re := regexp.MustCompile(`sitemap: (.*)`)
-	// re := regexp.MustCompile(`http://(.*)`)
-	submatchall := re.FindAllStringSubmatch(storeDomains, -1)
-	for _, element := range submatchall {
-		// fmt.Println(element[1])
-		response, err := http.Get(element[1])
+	trimdata := strings.Split(storeDomains, "\n")
+	// increase := 0
+	for _, url := range trimdata {
+		// increase++
+		// fmt.Fprintln(w,increase," " + url)
+
+		response, err := http.Get("https://" + url + "/robots.txt")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -42,30 +45,48 @@ func Domains() {
 			log.Fatal(err)
 		}
 		storeDomains := string(body)
-		re := regexp.MustCompile(`<loc>(.*)</loc>`)
+		re := regexp.MustCompile(`sitemap: (.*)`)
 		// re := regexp.MustCompile(`http://(.*)`)
 		submatchall := re.FindAllStringSubmatch(storeDomains, -1)
+		for _, element := range submatchall {
+			// fmt.Println(element[1])
+			response, err := http.Get(element[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer response.Body.Close()
+			// Read data from url
+			body, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			storeDomains := string(body)
+			re := regexp.MustCompile(`<loc>(.*)</loc>`)
+			// re := regexp.MustCompile(`http://(.*)`)
+			submatchall := re.FindAllStringSubmatch(storeDomains, -1)
 
-		//find last index of an string
-		lastindex := len(submatchall) - 1
-		urls := (submatchall[lastindex][1])
-		// fmt.Println(urls)
-		resp, err := http.Get(urls)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer resp.Body.Close()
-		bdy, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-		storeurls := string(bdy)
-		regx := regexp.MustCompile(`<loc>(.*)</loc>`)
-		submatch := regx.FindAllStringSubmatch(storeurls, -1)
-		// fmt.Println(storeurls)
-		for _, elements := range submatch {
+			//find last index of an string
+			lastindex := len(submatchall) - 1
+			urls := (submatchall[lastindex][1])
+			// fmt.Println(urls)
+			resp, err := http.Get(urls)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer resp.Body.Close()
+			bdy, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+			storeurls := string(bdy)
+			regx := regexp.MustCompile(`<loc>(.*)</loc>`)
+			submatch := regx.FindAllStringSubmatch(storeurls, -1)
+			lastindex = len(submatch) - 1
+			urls = (submatch[lastindex][1])
+			// fmt.Println(storeurls)
+			// for _, elements := range submatch {
 			// fmt.Println(elements[1])
-			if resp, err := http.Get(elements[1]); err == nil {
+			if resp, err := http.Get(urls); err == nil {
 				defer resp.Body.Close()
 
 				// log.Println("Load page complete")
@@ -74,8 +95,6 @@ func Domains() {
 					// log.Println("Page response is NOT nil")
 					// --------------
 					data, _ := ioutil.ReadAll(resp.Body)
-					resp.Body.Close()
-
 					hdata := strings.Replace(string(data), "<noscript>", "", -1)
 					hdata = strings.Replace(hdata, "</noscript>", "", -1)
 					// --------------
@@ -96,22 +115,22 @@ func Domains() {
 									// 	imgDataOriginal = element.Val
 									// }
 								}
-
-								re := strings.Replace(imgSrcUrl, "https://chaufferjob.us", "", -1)
+								fmt.Println(url + " ---------> " + imgSrcUrl)
+								// re := strings.Replace(imgSrcUrl, "http://"+url, "", -1)
 								// log.Println("imageUrl " + re)
 
-								resp, err := http.Get("https://chaufferjob.us" + re)
-								red := color.FgRed.Render
+								// resp, err := http.Get("https://" + url + re)
+								// red := color.FgRed.Render
 
-								if err != nil {
-									fmt.Println(err)
-								}
-								if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-									fmt.Println(re)
-									fmt.Print()
-								} else {
-									fmt.Println(red(re))
-								}
+								// if err != nil {
+								// fmt.Println(err)
+								// }
+								// if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+								// fmt.Println(re)
+								// fmt.Print()
+								// } else {
+								// fmt.Println(red(re))
+								// }
 								// resp.Body.Close()
 								// bodyElement,err := ioutil.ReadAll(resp)
 							}
@@ -131,6 +150,8 @@ func Domains() {
 					log.Println("Page response IS nil")
 				}
 			}
+			// }
+
 		}
 
 	}
