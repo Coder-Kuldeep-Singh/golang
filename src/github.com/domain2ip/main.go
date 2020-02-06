0a43b18bb742067179be38d7a0b5ed837c896805
mavensingh <kuldeep.avsar@gmail.com>
2020-02-06 13:02:32 +0530

    mopdified:added a txt file saver method

--- src/github.com/domain2ip/main.go
+++ src/github.com/domain2ip/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,12 +11,13 @@ import (
 	"strings"
 )
 
-// type Domains struct {
-// 	Domain []string `json:"domain"`
-// 	// IP     []int    `json:"ip"`
-// }
+type Domains struct {
+	Domain string `json:"domain"`
+	IP     string `json:"ip"`
+}
 
 func main() {
+	// var data []Domains
 	url, err := http.Get("http://s.tutree.com:7635/v1/groups")
 	Error(err)
 	defer url.Body.Close()
@@ -36,8 +38,13 @@ func main() {
 		for _, domains := range trim {
 			// fmt.Println(string(domains))
 			convertdomain2ip(string(domains))
+			// writeFile(domains)
 		}
 	}
+	// fmt.Println("file written successfully")
+	// write, _ := json.MarshalIndent(data, "", "")
+	// // fmt.Println(string(write))
+	// _ = ioutil.WriteFile("domain2ip.json", write, 0644)
 	os.Exit(0)
 }
 
@@ -47,21 +54,52 @@ func Error(err error) {
 	}
 }
 
+//Write data into json file
 func convertdomain2ip(name string) {
 	addr, err := net.ResolveIPAddr("ip", name)
 	if err != nil {
 		fmt.Println("Resolution error", err.Error())
 		// os.Exit()
 	}
-	// var data []Domains
-	// // for _, names := range name {
-	// data = append(data, Domains{
-	// 	Domain: []string{name},
-	// 	// IP:     []int{addr},
-	// })
-	// // }
-	// write, _ := json.MarshalIndent(data, "", "")
+	typeconvert := addr.String()
+	var data []Domains
+	// for _, names := range trimagain {
+	data = append(data, Domains{
+		Domain: name,
+		IP:     typeconvert,
+	})
+	// }
+	write, _ := json.MarshalIndent(data, "", "")
 	// fmt.Println(string(write))
-	// _ = ioutil.WriteFile("domain2ip.json", write, 0644)
+	_ = ioutil.WriteFile("domain2ip.json", write, 0644)
 	fmt.Println("domain=" + name + "\t" + "ip=" + addr.String())
 }
+
+//WriteFile function writing data into a txt file
+// func writeFile(data string) {
+// 	// addr, err := net.ResolveIPAddr("ip", data)
+// 	// if err != nil {
+// 	// 	fmt.Println("Resolution error", err.Error())
+// 	// 	// os.Exit()
+// 	// }
+// 	f, err := os.Create("address.txt")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		f.Close()
+// 		return
+// 	}
+// 	trim := string(data)
+// 	trimagain := strings.Split(trim, "\n")
+// 	for i, v := range trimagain {
+// 		fmt.Fprintln(f, v[i])
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 	}
+// 	err = f.Close()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// }
