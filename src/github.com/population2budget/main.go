c84dd99fc97d9b7ec30e407facea764c8e260c7a
mavensingh <kuldeep.avsar@gmail.com>
2020-02-10 19:27:27 +0530

    taking data from Populations table

--- src/github.com/population2budget/main.go
+++ src/github.com/population2budget/main.go
@@ -30,37 +30,49 @@ func CheckErrors(err error) {
 }
 
 // Offer represents a job
-type Offer struct {
-	ID             int
-	JobTitle       string
-	JobDescription string
+type Populations struct {
+	ID         int
+	Date       string
+	Jobtitle   string
+	City       string
+	population int
 }
 
-func getOffers() {
+func getPopulations() {
 	db := getDatabaseConnection()
-	dbrows, err := db.Query("select id, job_title, job_description from site")
+	dbrows, err := db.Query("select id,date, jobtitle, city, population from Populations")
 	if err != nil {
 		fmt.Println("BAD SQL", err)
 	}
 	// offers := []Offer{}
 	for dbrows.Next() {
 		var id int
+		var date string
 		var jobTitle string
-		var jobDescription string
-		err = dbrows.Scan(&id, &jobTitle, &jobDescription)
+		var city string
+		var poplations int
+		err = dbrows.Scan(&id, &date, &jobTitle, &city, &poplations)
 		if err != nil {
 			fmt.Println(err)
 		}
-		var offer Offer
-		offer.ID = id
-		offer.JobTitle = jobTitle
-		offer.JobDescription = jobDescription
-		fmt.Println(offer.ID, "------->", offer.JobTitle, "------->", offer.JobDescription)
+		var population Populations
+		population.ID = id
+		population.Date = date
+		population.Jobtitle = jobTitle
+		population.City = city
+		population.population = poplations
+		// fmt.Println(population.ID, "------->", population.Date, "------->", population.Jobtitle, "------->", population.City, "------->", population.population)
+		fmt.Println("ID : ", population.ID)
+		fmt.Println("Date : ", population.Date)
+		fmt.Println("JobTitle : ", population.Jobtitle)
+		fmt.Println("City : ", population.City)
+		fmt.Println("Population : ", population.population)
+		fmt.Println("***************************************************************************")
 		// offers = append(offers, offer)
 	}
 	// fmt.Println(offers)
 	db.Close()
 }
 func main() {
-	getOffers()
+	getPopulations()
 }
