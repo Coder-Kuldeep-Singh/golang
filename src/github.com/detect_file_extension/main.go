ba644e451b1a340e22ff839bcee3a4f22dfcd3d0
mavensingh <kuldeep.avsar@gmail.com>
2020-02-13 10:40:39 +0530

    tool to support all file extension

---
+++ src/github.com/detect_file_extension/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	filename := flag.String("f", "", "Provide the name|path of the file")
+	flag.Parse()
+	fileextension := filepath.Ext(*filename)
+	// fmt.Println(fileextension)
+	if fileextension == ".csv" {
+		CsvFileReader(*filename)
+	} else if fileextension == ".txt" {
+		TxtFileReader(*filename)
+	} else if fileextension == ".xml" {
+		XmlFileReader(*filename)
+	} else if fileextension == ".json" {
+		JsonFilereader(*filename)
+	} else {
+		fmt.Println("This Tool doesn't support " + fileextension + " file")
+	}
+
+	// fmt.Println("Extension of the file:", fileextension)
+}
+
+//CsvFileReader  reader the All data of the csv file
+func CsvFileReader(filename string) {
+	// Open the file
+	csvfile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	defer csvfile.Close()
+	// Parse the file
+	r := csv.NewReader(csvfile)
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record)
+	}
+}
+
+//TxtFileReader  reader the All data of the txt file
+func TxtFileReader(filename string) {
+	//Open the file
+	txtfile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("Couldn't open the txt file", err)
+	}
+	defer txtfile.Close()
+	var lines []string
+	//Parsing the txt file
+	scanner := bufio.NewScanner(txtfile)
+	//Iterate
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	fmt.Println(lines)
+}
+
+//XmlFileReader reader the All data of the xml file
+func XmlFileReader(filename string) {
+	//Open the file
+	xmlfile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("Couldn't open the xml file", err)
+	}
+	defer xmlfile.Close()
+	var lines []string
+	//Parsing the txt file
+	scanner := bufio.NewScanner(xmlfile)
+	//Iterate
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	fmt.Println(lines)
+}
+
+//JsonFileReader  reader the All data of the json file in array
+func JsonFilereader(filename string) {
+	//Open the file
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("Couldn't open the json file", err)
+	}
+	defer jsonFile.Close()
+	var lines []string
+	//Parsing the txt file
+	scanner := bufio.NewScanner(jsonFile)
+	//Iterate
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	fmt.Println(lines)
+}
