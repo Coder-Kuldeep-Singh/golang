f81a239f2c1ba9075d9e3a80443818389151c19b
mavensingh <kuldeep.avsar@gmail.com>
2020-02-18 02:49:50 -0800

    added new styles for header and footer

--- src/github.com/template_generate/main.go
+++ src/github.com/template_generate/main.go
@@ -9,10 +9,11 @@ import (
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
+
 )
 
 func getDatabaseConnection() *sql.DB {
-	connectionString := "root:12qwaszx@tcp(localhost:3306)/tutree_jobs"
+	connectionString := "root:@tcp(localhost:3306)/deliveryjobsnyc"
 	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		panic(err)
