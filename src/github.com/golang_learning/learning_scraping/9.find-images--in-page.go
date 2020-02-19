7fc5589d646b2a3b43ef9ca7147dccb302ade8da
mavensingh <kuldeep.avsar@gmail.com>
2020-02-19 17:21:06 +0530

    find images'

---
+++ src/github.com/golang_learning/learning_scraping/9.find-images--in-page.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func processElement(index int, element *goquery.Selection) {
+	//see if the href attribute exists on the element
+	imgSrc, exists := element.Attr("src")
+	if exists {
+		fmt.Println(imgSrc)
+	}
+}
+
+func main() {
+	url := flag.String("u", "", "Provide the url")
+	flag.Parse()
+	//make an http request
+	response, err := http.Get(*url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	//read response data in memory
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body", err.Error())
+		os.Exit(1)
+	}
+	document.Find("img").Each(processElement)
+
+}
